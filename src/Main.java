478901efc46fc36e2875181eb3898a56e5c154d5
Adefemi Kolawole <akolawole@ggc.edu>
2019-02-12 19:20:32 -0500

    after hours of trying to figure out. finally was able to get the questinos and answers printed and shuffled accordingly. We need to create a random to make sure the questions are not serially printed but also shuffled when player gets on the game scene

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,34 @@
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
         Quiz quiz = new Quiz();
         State st = new State();
-
         String user_input = quiz.getUserInput();
-
         String question = "";
         String answer = "";
         Random rand = new Random();
-
         int random;
-
         ArrayList<String> states = new ArrayList<>();//all the states
         ArrayList<String> capitals = new ArrayList<>(); // all the capitals
         ArrayList<String> questions;//all the questions
-        ArrayList<String> answers ; // all the answers
+        ArrayList<String> answers; // all the answers
         Map<String, String> state_and_capital;
         Map<String, String> question_and_answer;
-
-
         try {
             states = st.getAllExpressionInstance("//state");//store all states
             capitals = st.getAllExpressionInstance("//capital");// store all capitals
-
         } catch (Exception e) {
             e.printStackTrace();
         }
         System.out.println("New state: " + states);
-
         System.out.println("New capitals: " + capitals);
-
-
-
-
         questions = quiz.getQuestions(); // all the questions
-
         answers = quiz.getAnswers(); // all the answers
-
         state_and_capital = quiz.getState_and_capital(); //map that has state and capital in a key value pair
         question_and_answer = quiz.getQuestion_answer(); //map that has state and capital in a key value pair
-
         //check size of both array before creating a map
-
         int stateSize = states.size();
         int capitalSize = capitals.size();
         int questionSize = 0;
@@ -53,53 +36,42 @@ public class Main {
             questionSize = stateSize = capitalSize; // make sure the sized are same for stae, capital and the question generated from them
         else
             questionSize = 0;
-        for (int i =0; i < questionSize; i++){
+        for (int i = 0; i < questionSize; i++) {
             state_and_capital.put(states.get(i), capitals.get(i));
-
-       }
-
-        for (int i =0; i < questionSize; i++){
-            state_and_capital.put(states.get(i), capitals.get(i));
-
         }
-
-
+        for (int i = 0; i < questionSize; i++) {
+            state_and_capital.put(states.get(i), capitals.get(i));
+        }
         //print the new map to see if all states and capitals have been well mapped.
-
         System.out.println("State and Captial map");
-        for (Map.Entry entry : state_and_capital.entrySet())
-        {
-          //  System.out.println("State: " + entry.getKey() + "|| Capital: " + entry.getValue());
+        for (Map.Entry entry : state_and_capital.entrySet()) {
+            //  System.out.println("State: " + entry.getKey() + "|| Capital: " + entry.getValue());
         }
-
-        System.out.println("State size: " + states.size() );
-        System.out.println("Capital size: " + capitals.size() );
-
+        System.out.println("State size: " + states.size());
+        System.out.println("Capital size: " + capitals.size());
         //add all states and capitals into question
-        for (int i =0; i < questionSize; i++){
+        for (int i = 0; i < questionSize; i++) {
             question = quiz.generateQuestion(states, capitals, i);
             //System.out.println(question);
-            answer = quiz.generateAnswer(states.get(i), capitals.get(i));
+            // answer = quiz.generateAnswer();
             question_and_answer.put(question, answer);
         }
-
         System.out.println("Questions and Answers");
-        for (Map.Entry entry : question_and_answer.entrySet())
-        {
-         //   System.out.println("Question: " + entry.getKey() + "\nAnswer: " + entry.getValue());
+        for (Map.Entry entry : question_and_answer.entrySet()) {
+            //   System.out.println("Question: " + entry.getKey() + "\nAnswer: " + entry.getValue());
         }
-
-        System.out.println("Question size: " + states.size() );
-        System.out.println("Answer size: " + capitals.size() );
-
+        System.out.println("Question size: " + states.size());
+        System.out.println("Answer size: " + capitals.size());
         ArrayList<ArrayList<String>> qa = new ArrayList<>();
-
-
-       // answers = quiz.getContentOfMap(question_and_answer).get(1);
-
-        System.out.println("Questions alone");
-        System.out.println(quiz.generateQuestion(states, capitals, 45));
-        System.out.println(states.indexOf("Washington"));
-
+        // answers = quiz.getContentOfMap(question_and_answer).get(1);
+        for (int i = 0; i < states.size(); i++) {
+            System.out.println("Question No. " + i);
+            System.out.println(quiz.generateQuestion(states, capitals, i));
+            System.out.println("Answer for question No. " + i);
+            System.out.println(quiz.generateAnswer());
+        }
+        //  System.out.println(answer);
+        //System.out.println(quiz.getState());
+        // System.out.println(quiz.getState());
     }
 }
--- src/Quiz.java
+++ src/Quiz.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 public class Quiz {
     // private String question, answer;
     //private boolean playMode;
@@ -8,114 +7,90 @@ public class Quiz {
     private Map<String, String> question_answer;
     private final ArrayList<String> questions;
     private final ArrayList<String> answers;
-
+    private String correctAnswer;
+    private String state;
     public Quiz() {
         question_answer = new LinkedHashMap<>();
         questions = new ArrayList<>();
         answers = new ArrayList<>();
         question_answer = new LinkedHashMap<>();
         state_and_capital = new LinkedHashMap<>();
+        state = "";
+        correctAnswer = "";
     }
-
     public void readXML() {
     }
-
     public String generateQuestion(ArrayList<String> states, ArrayList<String> capitals, int index) {
-        String questionString ="";
-
+        String questionString = "";
         try {
-            int index1 = index ;
+            int index1 = index;
             int index2 = (index + 10) % states.size();
             int index3 = (index + 20) % states.size();
             int index4 = (index + 30) % states.size();
             int index5 = (index + 40) % states.size();
-
-
-            String state = states.get(index1);
-            String correctAnswer = capitals.get(states.indexOf(state)); //get index of current state and find the capital that matches it.
+            state = states.get(index1); //the current state about which the question is being asked
+            //get index of current state and find the capital that matches it.
+            correctAnswer = capitals.get(states.indexOf(state)); //the correct answer out of all the opotions that are made available.
             String wrongAnswer1 = capitals.get(index2);
             String wrongAnswer2 = capitals.get(index3);
             String wrongAnswer3 = capitals.get(index4);
             String wrongAnswer4 = capitals.get(index5);
-
-           //questionString =    formQuestionSentence( state, wrongAnswer1, wrongAnswer2,  wrongAnswer3, correctAnswer,wrongAnswer4);
-
-         // if (index > 20 && index <  30 )
-             //  questionString =    formQuestionSentence( state, correctAnswer, correctAnswer,  wrongAnswer3, correctAnswer,wrongAnswer4);
-        //  for (int i = states.indexOf(correctAnswer); i < states.size(); i++ ){
-            if( index  >-1  && index  < 10)
-               questionString =   formQuestionSentence( state, correctAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3, wrongAnswer4);
-
-
-           if(index  >10  && index  < 20)
-               questionString =    formQuestionSentence( state,  wrongAnswer1,correctAnswer,  wrongAnswer2, wrongAnswer3, wrongAnswer4);
-
-
-
-           if(index  >20  && index  < 30)
-                questionString =    formQuestionSentence( state, wrongAnswer1, wrongAnswer2, correctAnswer, wrongAnswer3, wrongAnswer4);
-
-           if(index  >30  && index  < 40)
-              questionString =    formQuestionSentence( state, wrongAnswer1, wrongAnswer2, wrongAnswer3, correctAnswer,wrongAnswer4);
-
-            if(index  >40  && index  < 50)
-                questionString =    formQuestionSentence( state, wrongAnswer1, wrongAnswer2, wrongAnswer3, wrongAnswer4, correctAnswer);
-
-
+            if (index > -1 && index < 10)
+                questionString = formQuestionSentence(state, correctAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3, wrongAnswer4);
+            if (index > 10 && index < 20)
+                questionString = formQuestionSentence(state, wrongAnswer1, correctAnswer, wrongAnswer2, wrongAnswer3, wrongAnswer4);
+            if (index > 20 && index < 30)
+                questionString = formQuestionSentence(state, wrongAnswer1, wrongAnswer2, correctAnswer, wrongAnswer3, wrongAnswer4);
+            if (index > 30 && index < 40)
+                questionString = formQuestionSentence(state, wrongAnswer1, wrongAnswer2, wrongAnswer3, correctAnswer, wrongAnswer4);
+            if (index > 40 && index < 50)
+                questionString = formQuestionSentence(state, wrongAnswer1, wrongAnswer2, wrongAnswer3, wrongAnswer4, correctAnswer);
             // questionString =    formQuestionSentence( state, wrongAnswer1, wrongAnswer2,  wrongAnswer3,wrongAnswer4, correctAnswer);
-
-           return questionString;
+            return questionString;
         } catch (Exception ex) {
-            return "Ensure you have all lists needed for this program to run.";
+            return "Error: CHeck Index: Ensure you have all lists needed for this program to run.";
         }
     }
-
-        public String formQuestionSentence(String state, String a, String b, String c, String d, String e){
-                    String questionString = "What is the capital of " + state + "?" + "\n" + "a) " + a + "\n" + "b)" +
-                    " " + b + "\n" + "c) " + c + "\n" + "d) " + d + "\n" + "e) " + e;
-
-
-            return questionString;
+    public String formQuestionSentence(String state, String a, String b, String c, String d, String e) {
+        String questionString = "What is the capital of " + state + "?" + "\n" + "a) " + a + "\n" + "b)" +
+                " " + b + "\n" + "c) " + c + "\n" + "d) " + d + "\n" + "e) " + e;
+        return questionString;
     }
-    public String generateAnswer(final String state, final String capital) {
-        // return "The capital of " + state + " is " + capital;
-        return "";
+    public String generateAnswer() {
+        String answer = "";
+        try {
+            answer = "The capital of " + state + " is " + correctAnswer;
+        } catch (Exception ex) {
+            System.out.println("Error: Check you index please!");
+        }
+        return answer;
     }
-
     public void checkAnswer(final String answer) {
         if (userInput.equals(answer))
             System.out.println("Correct");
         else
             System.out.println("Incorrect");
     }
-
     public void generateScore() {
+        }
+        public void returncorrectAnwers() {
+        }
+        public String getUserInput() {
+            return userInput;
     }
-
-    public void returncorrectAnwers() {
-    }
-
-    public String getUserInput() {
-        return userInput;
-    }
-
     public ArrayList<String> getQuestions() {
         return questions;
     }
-
     public ArrayList<String> getAnswers() {
         return answers;
     }
-
     public Map<String, String> getState_and_capital() {
         return state_and_capital;
     }
-
     public Map<String, String> getQuestion_answer() {
         return question_answer;
     }
-
-    public ArrayList<ArrayList<String>> getContentOfMap(Map<String, String> mapName) {
+    public ArrayList<ArrayList<String>> getContentOfMap(Map<String, String> mapName) { //read xml file and extract the necessary data
         mapName = new LinkedHashMap<>();
         ArrayList<ArrayList<String>> keys_and_values = new ArrayList<>();
         ArrayList<String> keys = new ArrayList<>();
@@ -126,10 +101,8 @@ public class Quiz {
             String value = mapName.get(key);
             keys.add(key);
             values.add(value);
-
             keys_and_values.add(keys);
             keys_and_values.add(values);
-
         }
         return keys_and_values;
     }
